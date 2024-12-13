4aeabf2a1c8e74f43f04a418ff3481367e0220e4
Muhammed <faraanshaik19@gmail.com>
2024-12-13 20:59:37 +0530

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Navbar from "./pages/Navbar";
 import TaskManager from "./pages/TaskManager";
-import Tasks from "./pages/Tasks";
 
 function App() {
   return (
     <>
-      {/* <TaskManager /> */}
       <Navbar />
-      <Tasks />
+      <TaskManager />
     </>
   );
 }
--- src/pages/TaskManager.jsx
+++ src/pages/TaskManager.jsx
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "./task_manager_styles.css";
+import axios from "axios";
 
 const TaskManager = () => {
   const [tasks, setTasks] = useState([]);
 
+  const getData = async () => {
+    await axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => setTasks(response.data.slice(0, 20)))
+      .catch((err) => console.log(err));
+  };
+
+  const deleteTask = async (id) => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  };
+
   useEffect(() => {
-    // Fetching data from API
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((data) => setTasks(data.slice(0, 20))); // Limiting to 20 tasks for simplicity
+    getData();
   }, []);
+
+  console.log(tasks);
 
   const handleStatusChange = (id, newStatus) => {
     setTasks((prevTasks) =>
@@ -20,39 +31,58 @@ const TaskManager = () => {
   };
 
   return (
-    <div className="task-container">
-      <h1 className="text-center">Task Manager</h1>
-      <table className="task-table">
-        <thead>
-          <tr>
-            <th>Task ID</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
-            <tr key={task.id}>
-              <td>{task.id}</td>
-              <td className="title-cell">{task.title}</td>
-              <td>{`Task Description for ID ${task.id}`}</td>
-              <td>
-                <select
-                  className="status-dropdown"
-                  value={task.status || "To Do"}
-                  onChange={(e) => handleStatusChange(task.id, e.target.value)}
-                >
-                  <option value="To Do">To Do</option>
-                  <option value="In Progress">In Progress</option>
-                  <option value="Done">Done</option>
-                </select>
-              </td>
+    <>
+      <div className="task-container">
+        <h2>The Tasks are Listed Below</h2>
+        <table className="task-table">
+          <thead>
+            <tr>
+              <th>Task ID</th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task.id}>
+                <td>{task.id}</td>
+                <td className="title-cell">{task.title}</td>
+                <td>{`Task Description for ID ${task.id}`}</td>
+                <td>
+                  <select
+                    className="status-dropdown"
+                    value={task.status || "To Do"}
+                    onChange={(e) =>
+                      handleStatusChange(task.id, e.target.value)
+                    }
+                  >
+                    <option value="To Do">To Do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Done">Done</option>
+                  </select>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-warning"
+                    style={{ margin: "10px" }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteTask(task.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
